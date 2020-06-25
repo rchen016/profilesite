d197b442c834795872d3fde8a50f24986716adb8
Ricky Chen <ricky.chen@flashtalking.com>
2020-06-24 17:58:48 -0700

    added 8 @media for different viewport for frame 1 and frame 3

--- js/fadeSection.js
+++ js/fadeSection.js
@@ -16,33 +16,33 @@
 		}
 	});
 
-	$(window).scroll(function() {
-		var top_of_element = $("#projectBoxes1");
-		var top_of_element = $("#projectBoxes1").offset().top;
-		var bottom_of_element = $("#projectBoxes1").offset().top + $("#projectBoxes1").outerHeight();
-		var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-		var top_of_screen = $(window).scrollTop();
-
-		if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-			// The element is visible, do something
-			$(".projectBoxes11").addClass("fadeInEffect12");
-			$(".projectBoxes12").addClass("fadeInEffect5");
-			$(".projectBoxes13").addClass("fadeInEffect5");
-			$(".projectBoxes14").addClass("fadeInEffect12");
-		}
-	});
-
-	$(window).scroll(function() {
-		var top_of_element = $("#projectBoxes2").offset().top;
-		var bottom_of_element = $("#projectBoxes2").offset().top + $("#projectBoxes2").outerHeight();
-		var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-		var top_of_screen = $(window).scrollTop();
-
-		if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-			// The element is visible, do something
-			$(".projectBoxes21").addClass("fadeInEffect12");
-			$(".projectBoxes22").addClass("fadeInEffect5");
-			$(".projectBoxes23").addClass("fadeInEffect5");
-			$(".projectBoxes24").addClass("fadeInEffect12");
-		}
-	});
+	// $(window).scroll(function() {
+	// 	var top_of_element = $("#projectBoxes1");
+	// 	var top_of_element = $("#projectBoxes1").offset().top;
+	// 	var bottom_of_element = $("#projectBoxes1").offset().top + $("#projectBoxes1").outerHeight();
+	// 	var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+	// 	var top_of_screen = $(window).scrollTop();
+	//
+	// 	if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	// 		// The element is visible, do something
+	// 		$(".projectBoxes11").addClass("fadeInEffect12");
+	// 		$(".projectBoxes12").addClass("fadeInEffect5");
+	// 		$(".projectBoxes13").addClass("fadeInEffect5");
+	// 		$(".projectBoxes14").addClass("fadeInEffect12");
+	// 	}
+	// });
+	//
+	// $(window).scroll(function() {
+	// 	var top_of_element = $("#projectBoxes2").offset().top;
+	// 	var bottom_of_element = $("#projectBoxes2").offset().top + $("#projectBoxes2").outerHeight();
+	// 	var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+	// 	var top_of_screen = $(window).scrollTop();
+	//
+	// 	if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	// 		// The element is visible, do something
+	// 		$(".projectBoxes21").addClass("fadeInEffect12");
+	// 		$(".projectBoxes22").addClass("fadeInEffect5");
+	// 		$(".projectBoxes23").addClass("fadeInEffect5");
+	// 		$(".projectBoxes24").addClass("fadeInEffect12");
+	// 	}
+	// });
