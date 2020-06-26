350e08fb986fb5b548fbf9832b9307cadb03a7d9
Ricky Chen <ricky.chen@flashtalking.com>
2020-06-25 17:01:36 -0700

    fixed some mobile image being blurry'

--- js/fadeSection.js
+++ js/fadeSection.js
@@ -15,7 +15,34 @@
 			$(".expSectionText").addClass("fadeInEffect9")
 		}
 	});
-
+	// $(window).scroll(function() {
+	// 	var top_of_element = $(".parallaxBottom").offset().top;
+	// 	var bottom_of_element = $(".parallaxBottom").offset().top + $(".parallaxBottom").outerHeight();
+	// 	var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+	// 	var top_of_screen = $(window).scrollTop();
+	//
+	// 	if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	// 		// The element is visible, do something
+	// 		console.log("Hello", document.getElementsByClassName("parallaxTop")[0].style.backgroundImage)
+	//
+	// 		if (typeof window.orientation !== 'undefined') {
+	// 			document.getElementsByClassName("parallaxTop::after")[0].style.backgroundImage = "url('https://i.imgur.com/2ZoKc0l.jpg')";
+	// 		 }
+	// 	}
+	// });
+	// $(window).scroll(function() {
+	// 	var top_of_element = $(".parallaxTop").offset().top;
+	// 	var bottom_of_element = $(".parallaxTop").offset().top + $(".parallaxTop").outerHeight();
+	// 	var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+	// 	var top_of_screen = $(window).scrollTop();
+	//
+	// 	if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	// 		// The element is visible, do something
+	// 		if (typeof window.orientation !== 'undefined') {
+	// 			document.getElementsByClassName("parallaxTop")[0].style.backgroundImage = "url('https://i.imgur.com/hJDI7if.jpg')";
+	// 		 }
+	// 	}
+	// });
 	// $(window).scroll(function() {
 	// 	var top_of_element = $("#projectBoxes1");
 	// 	var top_of_element = $("#projectBoxes1").offset().top;
